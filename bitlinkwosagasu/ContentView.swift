bfd948b794763f0dd0612bb332a0db9962876378
Ryo Tsuzukihashi <ryo.tsuzukihashi@zozo.com>
2024-08-23 15:39:18 +0900

    Initial Commit

---
+++ bitlinkwosagasu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bitlinkwosagasu
+//
+//  Created by ryo.tsuzukihashi on 2024/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bitlinkwosagasu/bitlinkwosagasuApp.swift
@@ -0,0 +1,17 @@
+//
+//  bitlinkwosagasuApp.swift
+//  bitlinkwosagasu
+//
+//  Created by ryo.tsuzukihashi on 2024/08/23.
+//
+
+import SwiftUI
+
+@main
+struct bitlinkwosagasuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
